85e1deab54dc27863493315e4960a50d48fa7fe3
neil aular <neilaular01@protonmail.com>
2023-01-12 21:43:20 -0300

    actualizacion numero 1

--- assets/js/jsparte2.js
+++ assets/js/jsparte2.js
@@ -1,21 +1,19 @@
-var inputGithub = document.querySelector("#CantidadGithub")
-var inputSlack = document.querySelector("#CantidadSlack")
-var inputGit = document.querySelector("#CantidadGit")
+var inputGithub = document.querySelector("#CantidadGithub");
+var inputSlack = document.querySelector("#CantidadSlack");
+var inputGit = document.querySelector("#CantidadGit");
 
-var spanCantidadTotal = document.querySelector("#cantidadTotal")
+var spanCantidadTotal = document.querySelector("#cantidadTotal");
 
-function calcularTotalStickers(){
-    var cantidadGithub = Number(inputGithub.value)
-    var cantidadSlack = Number(inputSlack.value)
-    var cantidadGit = Number(inputGit.value)
+function calcularTotalStickers() {
+  var cantidadGithub = Number(inputGithub.value);
+  var cantidadSlack = Number(inputSlack.value);
+  var cantidadGit = Number(inputGit.value);
 
+  var CantidadTotal = cantidadGithub + cantidadSlack + cantidadGit;
 
-    var CantidadTotal = cantidadGithub + cantidadSlack + cantidadGit
-
-    if(CantidadTotal <= 10){
-    spanCantidadTotal.innerHTML = CantidadTotal
-    }
-    else{
-        spanCantidadTotal.innerHTML = "demasiados"
-    }
+  if (CantidadTotal <= 10) {
+    spanCantidadTotal.innerHTML = CantidadTotal;
+  } else {
+    spanCantidadTotal.innerHTML = "demasiados";
+  }
 }
